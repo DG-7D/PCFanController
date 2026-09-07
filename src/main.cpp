8e98e585172f6d40cfb12e13811779f92ed2bea9
DG-7D <96519093+DG-7D@users.noreply.github.com>
2026-09-07 22:41:45 +0900

    多分シフトレジスタ殴れる

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,18 @@ constexpr uint16_t MIN_RPM = 120;
 constexpr uint16_t PWM_PERIOD_CLOCKS = F_CPU / PWM_FREQ;
 constexpr uint32_t CLK_TICKS_PER_MIN = F_CPU / PWM_PERIOD_CLOCKS * 60;
 constexpr uint32_t TIMEOUT_TICKS = CLK_TICKS_PER_MIN / MIN_RPM / 2;
+constexpr uint8_t LED_TICKS_PER_DIGIT = 64; // 1桁391Hz、4桁98Hz
 
 volatile uint16_t time = 0;
 volatile uint16_t lastPulse = 0;
 volatile uint16_t pulseWidth = 0;
+volatile bool ledUpdateFlag = true;
+uint8_t nextDigit = 0;
 
 int main() {
     _PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, ~CLKCTRL_PEN_bm);
+
+    PORTA.DIRSET = PIN1_bm | PIN3_bm | PIN4_bm;
 
     PORTB.DIRSET = PIN0_bm;
     PORTB.DIRCLR = PIN1_bm;
@@ -24,6 +29,9 @@ int main() {
     TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
     TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP0EN_bm | TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
     TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc | TCA_SINGLE_ENABLE_bm;
+
+    SPI0.CTRLB = ~SPI_BUFEN_bm | SPI_SSD_bm | SPI_MODE_0_gc;
+    SPI0.CTRLA = ~SPI_DORD_bm | SPI_MASTER_bm | ~SPI_CLK2X_bm | SPI_PRESC_DIV16_gc | SPI_ENABLE_bm;
 
     sei();
     while (1) {
@@ -36,7 +44,20 @@ int main() {
         } else {
             rpm = CLK_TICKS_PER_MIN / ticks / 2;
         }
-        TCA0.SINGLE.CMP0BUF = (uint32_t)PWM_PERIOD_CLOCKS * rpm / 2000;
+        if (ledUpdateFlag) {
+            ledUpdateFlag = false;
+            PORTA.OUTCLR = PIN4_bm;
+            SPI0.DATA = 1 << nextDigit;
+            nextDigit = (nextDigit + 1) % 4;
+            while (!(SPI0.INTFLAGS & SPI_IF_bm)) {
+                ;
+            }
+            SPI0.DATA = rpm / 8; // 仮
+            while (!(SPI0.INTFLAGS & SPI_IF_bm)) {
+                ;
+            }
+            PORTA.OUTSET = PIN4_bm;
+        }
     }
 }
 
@@ -50,6 +71,9 @@ ISR(PORTB_PORT_vect) {
 ISR(TCA0_OVF_vect) {
     TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
     time++;
+    if (time % LED_TICKS_PER_DIGIT == 0) {
+        ledUpdateFlag = true;
+    }
     if (time == lastPulse + TIMEOUT_TICKS) {
         pulseWidth = 0;
     }
